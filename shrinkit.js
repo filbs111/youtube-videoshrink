c6b1fbdae60961af7cd4906be1fdb59f9ba05a87
filbs111 <ed.filby@gmail.com>
2018-01-12 23:12:02 +0000

    observe mutations for instant resize

--- shrinkit.js
+++ shrinkit.js
@@ -1,17 +1,36 @@
 
-//window.addEventListener("load",function(evt){
-	setTimeout(resizeIt,1000);
-//});
+var observer = new MutationObserver(function(mutations) {
+	// For the sake of...observation...let's output the mutation to console to see how this all works
+	/*
+	console.log("observing mutations!");
+	mutations.forEach(function(mutation) {
+		console.log(mutation.type);
+		console.log(mutation);
+	});
+	*/
+	resizeIt();
+});
+ 
+var observerConfig = {
+	attributes: true, 
+	//childList: true, 
+	//characterData: true 
+};
+ 
+// Node, config
+// In this case we'll listen to all changes to body and child nodes
+var videoPlayer = document.evaluate('//div[@id="movie_player"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+console.log("targetNode: " + videoPlayer);
+observer.observe(videoPlayer, observerConfig);
+
 
 function resizeIt(){
-	setTimeout(resizeIt,1000);
 	
 	//find video element. note that this might not change, so can move outside loop
 	var videoElement = document.evaluate('//div[@class="html5-video-container"]//video', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 	//console.log(videoElement);
 	
 	//discover if controls are showing, only shrink if showing
-	var videoPlayer = document.evaluate('//div[@id="movie_player"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 	console.log(videoPlayer.className);
 	if (videoPlayer.className.match("ytp-autohide")){
 		videoElement.style.transform="";
