aaba1bb72ecfd52093b9feab159208df60a67c7a
filbs111 <ed.filby@gmail.com>
2018-01-13 00:08:00 +0000

    smooth css transition

--- shrinkit.js
+++ shrinkit.js
@@ -1,13 +1,5 @@
 
 var observer = new MutationObserver(function(mutations) {
-	// For the sake of...observation...let's output the mutation to console to see how this all works
-	/*
-	console.log("observing mutations!");
-	mutations.forEach(function(mutation) {
-		console.log(mutation.type);
-		console.log(mutation);
-	});
-	*/
 	resizeIt();
 });
  
@@ -16,25 +8,26 @@ var observerConfig = {
 	//childList: true, 
 	//characterData: true 
 };
- 
-// Node, config
-// In this case we'll listen to all changes to body and child nodes
+
 var videoPlayer = document.evaluate('//div[@id="movie_player"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 console.log("targetNode: " + videoPlayer);
 observer.observe(videoPlayer, observerConfig);
 
 
 function resizeIt(){
-	
-	//find video element. note that this might not change, so can move outside loop
 	var videoElement = document.evaluate('//div[@class="html5-video-container"]//video', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 	//console.log(videoElement);
-	
-	//discover if controls are showing, only shrink if showing
-	console.log(videoPlayer.className);
-	if (videoPlayer.className.match("ytp-autohide")){
-		videoElement.style.transform="";
+	var elementClassList = videoElement.classList;
+	var playerClassList = videoPlayer.classList;
+
+	console.log(playerClassList);
+	if (playerClassList.contains("ytp-autohide")){
+		if (elementClassList.contains("shrunken")){
+			elementClassList.remove("shrunken");
+		}
 	}else{
-		videoElement.style.transform="scale(0.9,0.9)";
+		if (!elementClassList.contains("shrunken")){
+			elementClassList.add("shrunken");
+		}
 	}
 }
